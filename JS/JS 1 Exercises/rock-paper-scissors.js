31572ebf2d1aea77d70dcfe7655450b34ea07fd0
aaj90 <48999110+aaj90@users.noreply.github.com>
2023-10-27 23:09:27 +0300

    rock-paper-scissors.js

--- JS/JS 1 Exercises/rock-paper-scissors.js
+++ JS/JS 1 Exercises/rock-paper-scissors.js
@@ -1,29 +1,22 @@
 
-
-console.log(1);
-
-
-
-
-
-var rock = document.getElementById("rock").addEventListener("click", function(){play(1);});
-var paper = document.getElementById("paper").addEventListener("click", function(){play(2);});
-var scissors = document.getElementById("scissors").addEventListener("click", function(){play(3);});
+document.getElementById("rock").addEventListener("click", function(){play(1);});
+document.getElementById("paper").addEventListener("click", function(){play(2);});
+document.getElementById("scissors").addEventListener("click", function(){play(3);});
 
 
 function addImg(c,player){
     
     if(player == "p1"){
         if(c==1){
-            let code = "<img id=\"p1\"  src=\"C\:\\Users\\Admin\\Desktop\\KFUPM\\231\\SWE 363\\swe-363-231\\JS\\Pics\\rock.png\" alt=\"rock\">";
+            let code = "<img id=\"p1\"  src=\"https://github.com/aaj90/swe-363-231/blob/main/JS/Pics/rock.png?raw=true\" alt=\"rock\">";
             console.log(code);
             document.getElementById("Hands").insertAdjacentHTML("afterbegin",code);
             }else if(c == 2){
-                let code = "<img id=\"p1\"  src=\"C\:\\Users\\Admin\\Desktop\\KFUPM\\231\\SWE 363\\swe-363-231\\JS\\Pics\\paper.png\" alt=\"paper\">";
+                let code = "<img id=\"p1\"  src=\"https://github.com/aaj90/swe-363-231/blob/main/JS/Pics/paper.png?raw=true\" alt=\"paper\">";
                 console.log(code);
                 document.getElementById("Hands").insertAdjacentHTML("afterbegin",code);
             }else{
-                let code = "<img id=\"p1\"  src=\"C\:\\Users\\Admin\\Desktop\\KFUPM\\231\\SWE 363\\swe-363-231\\JS\\Pics\\scissors.png\" alt=\"scissors\">";
+                let code = "<img id=\"p1\"  src=\"https://github.com/aaj90/swe-363-231/blob/main/JS/Pics/scissors.png?raw=true\" alt=\"scissors\">";
                 console.log(code);
                 document.getElementById("Hands").insertAdjacentHTML("afterbegin",code);
             }
@@ -31,15 +24,15 @@ function addImg(c,player){
 
     if(player == "p2"){
         if(c==1){
-            let code = "<img id=\"p2\"  src=\"C\:\\Users\\Admin\\Desktop\\KFUPM\\231\\SWE 363\\swe-363-231\\JS\\Pics\\rock.png\" alt=\"rock\">";
+            let code = "<img id=\"p2\"  src=\"https://github.com/aaj90/swe-363-231/blob/main/JS/Pics/rock.png?raw=true\" alt=\"rock\">";
             console.log(code);
             document.getElementById("Hands").insertAdjacentHTML("afterbegin",code);
             }else if(c == 2){
-                let code = "<img id=\"p2\"  src=\"C\:\\Users\\Admin\\Desktop\\KFUPM\\231\\SWE 363\\swe-363-231\\JS\\Pics\\paper.png\" alt=\"paper\">";
+                let code = "<img id=\"p2\"  src=\"https://github.com/aaj90/swe-363-231/blob/main/JS/Pics/paper.png?raw=true\" alt=\"paper\">";
                 console.log(code);
                 document.getElementById("Hands").insertAdjacentHTML("afterbegin",code);
             }else{
-                let code = "<img id=\"p2\"  src=\"C\:\\Users\\Admin\\Desktop\\KFUPM\\231\\SWE 363\\swe-363-231\\JS\\Pics\\scissors.png\" alt=\"scissors\">";
+                let code = "<img id=\"p2\"  src=\"https://github.com/aaj90/swe-363-231/blob/main/JS/Pics/scissors.png?raw=true\" alt=\"scissors\">";
                 console.log(code);
                 document.getElementById("Hands").insertAdjacentHTML("afterbegin",code);
             }
@@ -62,19 +55,19 @@ function play(p1){
         addImg(p2,"p2");
         addImg(p1,"p1");
         
-        let d = "<img id=\"winOrLose\"  src=\"C\:\\Users\\Admin\\Desktop\\KFUPM\\231\\SWE 363\\swe-363-231\\JS\\Pics\\L.png\" alt=\"Loser\">";
+        let d = "<img id=\"winOrLose\"  src=\"https://github.com/aaj90/swe-363-231/blob/main/JS/Pics/L.png?raw=true\" alt=\"Loser\">";
         document.getElementById("Hands").insertAdjacentHTML("afterbegin",d);
     }else if((p1==1 && p2==3) || (p1==2 && p2==1) ||(p1==3 && p2==2)){
         addImg(p2,"p2");
         addImg(p1,"p1");
         
-        let d = "<img id=\"winOrLose\"  src=\"C\:\\Users\\Admin\\Desktop\\KFUPM\\231\\SWE 363\\swe-363-231\\JS\\Pics\\W.png\" alt=\"Winner\">";
+        let d = "<img id=\"winOrLose\"  src=\"https://github.com/aaj90/swe-363-231/blob/main/JS/Pics/W.png?raw=true\" alt=\"Winner\">";
         document.getElementById("Hands").insertAdjacentHTML("afterbegin",d);
     }else{
         addImg(p2,"p2");
         addImg(p1,"p1");
         
-        let d = "<img id=\"winOrLose\"  src=\"C\:\\Users\\Admin\\Desktop\\KFUPM\\231\\SWE 363\\swe-363-231\\JS\\Pics\\D.png\" alt=\"Draw\">";
+        let d = "<img id=\"winOrLose\"  src=\"https://github.com/aaj90/swe-363-231/blob/main/JS/Pics/D.png?raw=true\" alt=\"Draw\">";
         document.getElementById("Hands").insertAdjacentHTML("afterbegin",d);
     }
 
